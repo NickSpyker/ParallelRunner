8f5f005968e0372d5143ef5d5bd26911f9bf6f8b
Nicolas Spijkerman <nicolas.spijkerman@epitech.eu>
2023-03-25 14:17:12 +0100

    add player movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,12 +1,28 @@
 extends KinematicBody2D
 
+export var gravity = 500
+export var jumpPower = 400
+
+var rng = RandomNumberGenerator.new()
+var velocity = Vector2.ZERO
+
 var playerType = "mask_"
 
 func _ready():
-	pass # Replace with function body.
+	rng.randomize()
+	var player = ["mask_", "ninja_", "pink_", "virtual_"]
+	playerType = player[rng.randi_range(0, 3)]
+	$AnimatedSprite.play(playerType + "run")
+	velocity = Vector2.ZERO
 
-#func _process(delta):
-#	pass
+func _process(delta):
+	if !is_on_floor():
+		velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func Jump():
+	if is_on_floor():
+		velocity.y = -jumpPower
 
 func setPlayerType(player: String):
 	match player:
