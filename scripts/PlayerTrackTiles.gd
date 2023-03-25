96a0cbf3aa56315c544b1aadf7925405c182ce4b
Nicolas Spijkerman <nicolas.spijkerman@epitech.eu>
2023-03-25 16:40:52 +0100

    add exit button

--- scripts/PlayerTrackTiles.gd
+++ scripts/PlayerTrackTiles.gd
@@ -1,13 +1,37 @@
 extends TileMap
 
 export var scrollSpeed = 150
-var mapWidth = 1280
+var mapPXWidth = 1280
+
+var mapWidth = null
+var mapHeight = null
+
+func getMap():
+	var map = []
+	for y in range(mapHeight):
+		var row = []
+		for x in range(mapWidth):
+			var cell = get_cell(x, y)
+			row.append(cell)
+		map.append(row)
+	return map
 
 func _ready():
-	pass
+	mapWidth = Global.MAP_WIDTH
+	mapHeight = Global.MAP_HEIGHT
+	var file = File.new()
+	file.open("res://file.txt", File.WRITE)
+	var map = getMap()
+	var s = ""
+	for row in map:
+		for element in row:
+			s += str(element) + " "
+		s += "\n"
+	file.store_string(s)
+	file.close()
 
 func _process(delta):
 	
 	position.x -= scrollSpeed * delta
-	if position.x < -mapWidth:
-		position.x += mapWidth
+	if position.x < -mapPXWidth:
+		position.x += mapPXWidth
--- scripts/global.gd
+++ scripts/global.gd
@@ -2,3 +2,6 @@ extends Node
 
 var playerType1 = null
 var playerType2 =  null
+
+const MAP_WIDTH = 64
+const MAP_HEIGHT = 9
