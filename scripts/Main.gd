f0cdd20c2e77bd9d2ff393b754d9f5c5e39bc66b
Nicolas Spijkerman <nicolas.spijkerman@epitech.eu>
2023-03-25 16:11:32 +0100

    add exit button

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -48,3 +48,7 @@ func _on_StartButton_pressed():
 	var isErr = get_tree().change_scene("res://scenes/Track.tscn")
 	if isErr:
 		OS.alert("An error has occurred, please restart the game.", "Error")
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
