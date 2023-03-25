ac16ea6e5f3aad9447dc54ffc245d45ecf49d5f5
Nicolas Spijkerman <nicolas.spijkerman@epitech.eu>
2023-03-25 15:53:36 +0100

    add players selection

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -3,14 +3,20 @@ extends Node2D
 export var parallaxSpeed = 50
 var parallax = 0
 
+var players = ["mask", "ninja", "pink", "virtual"]
+
+var index1 = null
+var index2 = null
+
 var rng = RandomNumberGenerator.new()
 func _ready():
 	rng.randomize()
-	var player = ["mask", "ninja", "pink", "virtual"]
-	Global.playerType1 = player[rng.randi_range(0, 3)]
-	Global.playerType2 = player[rng.randi_range(0, 3)]
-	while Global.playerType1 == Global.playerType2:
-		Global.playerType2 = player[rng.randi_range(0, 3)]
+	index1 = rng.randi_range(0, 3)
+	index2 = rng.randi_range(0, 3)
+	while index1 == index2:
+		index2 = rng.randi_range(0, 3)
+	Global.playerType1 = players[index1]
+	Global.playerType2 = players[index2]
 	$Player1.setPlayerType(Global.playerType1)
 	$Player2.setPlayerType(Global.playerType2)
 
@@ -19,6 +25,24 @@ func _process(delta):
 	if 1280 <= parallax:
 		parallax = 0
 	$bg.region_rect = Rect2(parallax, 0, 1280, 720)
+	
+	if Input.is_action_pressed("player1_jump"):
+		$Player1.Jump()
+	if Input.is_action_pressed("player2_jump"):
+		$Player2.Jump()
+		
+	if Input.is_action_just_pressed("player1_chose"):
+		index1 += 1
+		if index1 > 3:
+			index1 = 0
+		Global.playerType1 = players[index1]
+		$Player1.setPlayerType(Global.playerType1)
+	if Input.is_action_just_pressed("player2_chose"):
+		index2 += 1
+		if index2 > 3:
+			index2 = 0
+		Global.playerType2 = players[index2]
+		$Player2.setPlayerType(Global.playerType2)
 
 func _on_StartButton_pressed():
 	var isErr = get_tree().change_scene("res://scenes/Track.tscn")
